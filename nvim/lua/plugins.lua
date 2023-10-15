5b0f40f3983592f57d859da79a59ba88c5cf1756
james.wood <jwooddev@gmail.com>
2023-10-14 23:52:01 -0500

    update nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -32,4 +32,17 @@ return {
 
 	-- Fugitive
 	{"tpope/vim-fugitive"},
+
+    { "ray-x/go.nvim",
+        dependencies = {
+            "ray-x/guihua.lua",
+        },
+        config = function()
+            require("go").setup()
+        end,
+        event = {"CmdlineEnter"},
+        ft = {"go", 'gomod'},
+        build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+    }
+
 }
