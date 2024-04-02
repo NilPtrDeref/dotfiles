7425d47ac51e5a618691348640008fa2189f16e8
james.wood <jwooddev@gmail.com>
2024-04-01 23:23:33 -0500

    add terminal commands

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -14,8 +14,6 @@ vim.keymap.set('n', '<leader>qn', '<cmd>cnext<CR>', { desc = 'Go to next [Q]uick
 vim.keymap.set('n', '<leader>qo', vim.diagnostic.setloclist, { desc = '[O]pen diagnostic [Q]uickfix list' })
 vim.keymap.set('n', '<leader>qc', '<cmd>cclose<CR>', { desc = '[C]lose diagnostic [Q]uickfix list' })
 
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -27,6 +25,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+vim.keymap.set('n', '<C-down>', '<C-w>-', { desc = 'Shrink window vertically' })
+vim.keymap.set('n', '<C-up>', '<C-w>+', { desc = 'Grow window vertically' })
 
 -- Set directory viewer (replaced by oil)
 -- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Go to file browser' })
@@ -36,3 +36,7 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down half page' })
 vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up half page' })
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Move to next search match' })
 vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Move to previous search match' })
+
+-- Terminal
+vim.keymap.set('n', '<C-t>', '<cmd>15split term://fish<CR>', { desc = 'Open a terminal under the current window' })
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
