96814ec2c248973b0ada25c79ea8c04a77db2ee2
james.wood <jwooddev@gmail.com>
2024-04-19 02:33:03 -0500

    better terminal commands

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -25,8 +25,8 @@ vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 -- Split navigation
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the bottom window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the top window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<C-down>', '<C-w>-', { desc = 'Shrink window vertically' })
 vim.keymap.set('n', '<C-up>', '<C-w>+', { desc = 'Grow window vertically' })
 
@@ -42,3 +42,7 @@ vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Move to previous search match' })
 -- Terminal
 -- vim.keymap.set('n', '<C-t>', '<cmd>15split term://fish<CR>', { desc = 'Open a terminal under the current window' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C-h>', '<Cmd>wincmd h<CR>', { desc = 'Move focus to the left window' })
+vim.keymap.set('t', '<C-l>', '<Cmd>wincmd l<CR>', { desc = 'Move focus to the right window' })
+vim.keymap.set('t', '<C-j>', '<Cmd>wincmd j<CR>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('t', '<C-k>', '<Cmd>wincmd k<CR>', { desc = 'Move focus to the upper window' })
