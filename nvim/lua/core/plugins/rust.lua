28749b0754beaa84dfd2994ce3652ee63d4cd649
james.wood <jwooddev@gmail.com>
2024-04-20 20:59:32 -0500

    rust stuff

--- nvim/lua/core/plugins/rust.lua
+++ nvim/lua/core/plugins/rust.lua
@@ -4,5 +4,9 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^4',
     ft = { 'rust' },
+    config = function()
+      vim.keymap.set('n', '<leader>co', '<cmd>RustLsp openDocs<CR>', { desc = 'Open rust docs' })
+      vim.keymap.set('n', '<leader>cd', '<cmd>RustLsp renderDiagnostic<CR>', { desc = 'Render rust diagnostic' })
+    end,
   },
 }
