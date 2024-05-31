ceab3894f33d1a02ff838b06693fbb26bd3d2c05
james.wood <jwooddev@gmail.com>
2024-05-31 14:10:27 -0500

    colorscheme stuff

--- nvim/lua/core/plugins/colorscheme.lua
+++ nvim/lua/core/plugins/colorscheme.lua
@@ -16,7 +16,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'nightfox'
+      vim.cmd.colorscheme 'everforest'
       -- vim.opt.background = 'light'
       vim.cmd.hi 'Comment gui=none'
     end,
