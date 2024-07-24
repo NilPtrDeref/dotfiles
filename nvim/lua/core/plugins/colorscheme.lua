2026658b6fac8e2ec2449d62f6786ff48574f0b7
james.wood <jwooddev@gmail.com>
2024-07-24 09:54:49 -0500

    update config

--- nvim/lua/core/plugins/colorscheme.lua
+++ nvim/lua/core/plugins/colorscheme.lua
@@ -12,11 +12,13 @@ return {
       'sainnhe/gruvbox-material',
       'AlexvZyl/nordic.nvim',
       'doums/darcula',
+      'rktjmp/lush.nvim',
+      'ab-dx/ares.nvim',
     },
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'ares'
       -- vim.opt.background = 'light'
       vim.cmd.hi 'Comment gui=none'
     end,
