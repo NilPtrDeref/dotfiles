aa58fd9782a8c7eb131ba691ee30574b53f57ccb
jwood <jwooddev@gmail.com>
2025-01-12 00:56:41 -0600

    remove autohover

--- nvim/lua/core/plugins/lsp.lua
+++ nvim/lua/core/plugins/lsp.lua
@@ -122,24 +122,24 @@ return {
       vim.diagnostic.config { float = { border = border } }
       require('lspconfig.ui.windows').default_options = { border = border }
 
-      -- Autohighlight hovered word
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('framework-lsp-attach', { clear = true }),
-        callback = function(event)
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              callback = vim.lsp.buf.clear_references,
-            })
-          end
-        end
-      })
+      -- -- Autohighlight hovered word
+      -- vim.api.nvim_create_autocmd('LspAttach', {
+      --   group = vim.api.nvim_create_augroup('framework-lsp-attach', { clear = true }),
+      --   callback = function(event)
+      --     local client = vim.lsp.get_client_by_id(event.data.client_id)
+      --     if client and client.server_capabilities.documentHighlightProvider then
+      --       vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+      --         buffer = event.buf,
+      --         callback = vim.lsp.buf.document_highlight,
+      --       })
+      --
+      --       vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+      --         buffer = event.buf,
+      --         callback = vim.lsp.buf.clear_references,
+      --       })
+      --     end
+      --   end
+      -- })
     end
   },
 
