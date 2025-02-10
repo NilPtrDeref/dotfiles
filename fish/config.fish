5b8660632f7191b6c969fe1d52c0907a2a31b6a9
James Wood <jwooddev@gmail.com>
2025-02-10 01:30:43 -0600

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    alias vim "nvim"
+    alias tmuxa "tmux attach-session"
+    set -Ux PYENV_ROOT $HOME/.pyenv
+
+    # $HOME/opt/zls/zig-out/bin
+    set -U fish_user_paths $HOME/go/bin $PYENV_ROOT/bin $fish_user_paths
+    pyenv init - | source
+end
