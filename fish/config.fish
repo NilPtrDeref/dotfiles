88a58f1859c105f0b3de6f6d7140fa6ebd36ca17
James Wood <jwooddev@gmail.com>
2025-02-14 06:51:47 -0600

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,11 @@ if status is-interactive
     set fish_greeting
     alias vim "nvim"
     alias tmuxa "tmux attach-session"
-    set -Ux PYENV_ROOT $HOME/.pyenv
+    set PYENV_ROOT $HOME/.pyenv
 
     # $HOME/opt/zls/zig-out/bin
     set -U fish_user_paths $HOME/go/bin $PYENV_ROOT/bin $fish_user_paths
     pyenv init - | source
     set EDITOR nvim
-    set -Ux MANPAGER 'nvim +Man!'
+    set MANPAGER 'nvim +Man!'
 end
