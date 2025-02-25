babfa0537809af6cfaa653794e6cc106f88de279
James Wood <jwooddev@gmail.com>
2025-02-25 09:01:17 -0600

    update lazygit integration

--- nvim/lua/core/plugins/terminal.lua
+++ nvim/lua/core/plugins/terminal.lua
@@ -10,16 +10,8 @@ return {
         fish:toggle(20)
       end
 
-      local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float' }
-
-      function _lazygit_toggle()
-        lazygit:toggle(20)
-      end
-
       vim.keymap.set('n', '<C-t>', '<cmd>lua _fish_toggle()<CR>', { desc = 'Open a terminal under the current window' })
       vim.keymap.set('t', '<C-t>', '<cmd>lua _fish_toggle()<CR>', { desc = 'Close a terminal under the current window' })
-      vim.keymap.set('n', '<C-g>', '<cmd>lua _lazygit_toggle()<CR>', { desc = 'Open a lazygit terminal under the current window' })
-      vim.keymap.set('t', '<C-g>', '<cmd>lua _lazygit_toggle()<CR>', { desc = 'Close a lazygit terminal under the current window' })
     end,
   },
 }
