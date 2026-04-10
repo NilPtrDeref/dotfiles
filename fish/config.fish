0f1dd657c4c1397b733a4f98c9b5046d4639f1bb
James Wood <jwooddev@gmail.com>
2026-04-10 17:34:12 -0400

    Update Fish config with new aliases and functions
    Refactor Fish shell configuration with new aliases and functions.

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,54 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting
-    alias vim "nvim"
-    alias v "nvim"
+    fish_vi_key_bindings
+    alias vim nvim
+    alias v nvim
     alias tmuxa "tmux attach-session"
-    alias l "lazygit"
-    alias y "yazi"
-    alias emsdk_setup ". /home/jwood/opt/emsdk/emsdk_env.fish"
-    alias iclaude "ENABLE_IDE_INTEGRATION=true claude"
-    set -Ux PYENV_ROOT $HOME/.pyenv
+    alias l lazygit
+    alias y yazi
 
-    # $HOME/opt/zls/zig-out/bin
-    set -U fish_user_paths $HOME/opt/zig $HOME/go/bin $PYENV_ROOT/bin $fish_user_paths
-    pyenv init - | source
+    set fish_user_paths $HOME/go/bin $fish_user_paths
+    set fish_user_paths $HOME/Opt/bin $fish_user_paths
+    set fish_user_paths /usr/lib/emscripten $fish_user_paths
+    set fish_user_paths $HOME/Opt/zig/build/stage3/bin $fish_user_paths
+    set fish_user_paths $HOME/.local/bin $fish_user_paths
+    set fish_user_paths $HOME/Opt/zig/build/stage3/bin $fish_user_paths
+    # set fish_user_paths $HOME/Opt/zig-0.16.0-dev $fish_user_paths
+    # set fish_user_paths $HOME/Opt/zig-0.15.2 $fish_user_paths
+
     set EDITOR nvim
     set -Ux MANPAGER 'nvim +Man!'
 end
 
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/home/jwood/opt/google-cloud-sdk/path.fish.inc' ]; . '/home/jwood/opt/google-cloud-sdk/path.fish.inc'; end
+function n --wraps nvim --description 'alias n=nvim'
+    if count $argv >/dev/null
+        nvim -n $argv
+    else
+        nvim -n .
+    end
+end
+
+function tn --wraps trax --description 'alias tl=trax'
+    if count $argv >/dev/null
+        trax new $argv
+    else
+        trax new
+    end
+end
+
+function tl --wraps trax --description 'alias tl=trax'
+    if count $argv >/dev/null
+        trax ls $argv
+    else
+        trax ls
+    end
+end
+
+function te --wraps trax --description 'alias tl=trax'
+    if count $argv >/dev/null
+        trax edit $argv
+    else
+        trax edit
+    end
+end
